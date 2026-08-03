ca53ecf280142cce20fb7fbc8c183a0ef13340c8
Maxim Waider Korbut <130697194+MaximKorbut@users.noreply.github.com>
2026-08-03 15:38:37 +0300

    Calculate portfolio value
    Calculate portfolio value

--- frontend/app.js
+++ frontend/app.js
@@ -25,3 +25,51 @@ async function loadPrices(){
 }
 
 loadPrices();
+let prices = {};
+
+async function loadPrices(){
+
+    const response = await fetch("http://127.0.0.1:8000/prices");
+
+    prices = await response.json();
+
+    document.getElementById("prices").innerHTML=
+`
+<div class="card">
+
+<h2>Bitcoin</h2>
+<p>$${prices.bitcoin.usd}</p>
+
+<h2>Ethereum</h2>
+<p>$${prices.ethereum.usd}</p>
+
+<h2>Solana</h2>
+<p>$${prices.solana.usd}</p>
+
+</div>
+`;
+
+}
+
+function calculatePortfolio(){
+
+const btc =
+Number(document.getElementById("btc").value);
+
+const eth =
+Number(document.getElementById("eth").value);
+
+const sol =
+Number(document.getElementById("sol").value);
+
+const total =
+btc*prices.bitcoin.usd+
+eth*prices.ethereum.usd+
+sol*prices.solana.usd;
+
+document.getElementById("total").innerHTML=
+"$"+total.toFixed(2);
+
+}
+
+loadPrices();
