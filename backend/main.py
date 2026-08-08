01145430dd444a44a3e2f13f3e15ef3a3ba42ee0
Maxim Waider Korbut <130697194+MaximKorbut@users.noreply.github.com>
2026-08-08 17:49:23 +0300

    Add historical price endpoint
    Add historical price endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from prices import get_prices, get_history
 app = FastAPI(
     title="Crypto Portfolio API",
     version="1.0.0"
@@ -13,3 +13,6 @@ def home():
 "version":"2.0",
 "service":"Crypto Portfolio API"
 }
+@app.get("/history/{coin}")
+def history(coin: str):
+    return get_history(coin)
